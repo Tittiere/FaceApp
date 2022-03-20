a2e09f07fa65169651e8ee81cd6f53aecba3601f
Mattia <78874321+claristorio@users.noreply.github.com>
2022-03-20 17:52:26 +0100

    Update buildDatabase.py

--- buildDatabase.py
+++ buildDatabase.py
@@ -1,48 +1,83 @@
-import face_recognition, pickle, cv2, os
+import face_recognition, pickle, time, cv2, os
 
-path = r"C:\Users\matti\Desktop\FOTO SCIENZE\\"
+# funzione per pulire la shell
+def clear():
+    # scelgo il comando in base al sistema operativo
+    if str(os.path.sep) == '/':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+# chiedo all'utente il path della cartella che contiene il
+# database e rimuovo le virgolette in caso ci siano
+path = input('Inserisci il path alla cartella contenente il database:\n')
+time.sleep(1.5)
+if path.startswith('"') or path.startswith("'"):
+    path = path[1:-1]
+# aggiungo il separatore path di sistema
+path += os.path.sep
+# trovo, in ordine alfabetico, i nomi di tutto quello
+# che è contenutonella cartella del database
 filesInData = os.listdir(path)
 
+# inizializzo gli array che conterranno i valori dei volti
+# codificati ed i nomi corrispettivi
 knownEncodings = []
-knownNames = ['.']
+knownNames = []
 
-for k in filesInData:
-    if k.find('.') == -1:
-        dirPath = path + k + '\\'
+# algoritmo inutilmente complicato per visualizzare il progresso
+# è esattamente lo stesso procedimento spiegato dopo, serve a contare
+# quante immagini ci sono per stampare un progresso nel ciclo dopo
+totEls = 0
+for a in filesInData:
+    if a.find('.') == -1:
+        dirPath = path + a + os.path.sep
         filesInDir = os.listdir(dirPath)
+        elements = [dirPath + e for e in filesInDir if not (e.endswith('.ini')) and not (e.endswith('.py')) and not (e.endswith('.xml'))]
+        for b in elements:
+            totEls += 1
+
+# inizializzo una variabile per contare le immagini
+count = 0
+# per ogni elemento k nei files nel database
+for k in filesInData:
+    # se l'elemento k ha un punto nel nome significa che non è
+    # una cartella. Se è una cartella allora:
+    if k.find('.') == -1:
+        # creo il path della cartella aggiungendo il nome della cartella al path
+        dirPath = path + k + os.path.sep
+        # ottengo le immagini nella cartella corrispondente al nome
+        filesInDir = os.listdir(dirPath)
+        # creo un array con i precorsi integrali di tutte le immagini nella cartella
+        # escludendo eventuali file di config della cartella o file python
         imgPaths = [dirPath + e for e in filesInDir if not (e.endswith('.ini')) and not (e.endswith('.py')) and not (e.endswith('.xml'))]
+        # ciclo for che scorre le immagini percorso per percorso numerandole
         for (i, el)  in enumerate(imgPaths):
-            name = el.split(os.path.sep)[-2]
-            if len(name.split('_')) == 3:
-                name = name.split('_')
-                name = name[0] + ' ' + name[2]
-            print(name)
-            # load the input image and convert it from BGR (OpenCV ordering)
-            # to dlib ordering (RGB)
+            # pulisco la shell
+            clear()
+            # stampo il progresso
+            count += 1
+            print(f'{count}/{totEls}')
+            # carico l'immagine e la converto da BGR a un formato
+            # che la libreria "dlib" possa usare (RGB)
             image = cv2.imread(el)
             rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            #Use Face_recognition to locate faces
+            # utilizzo l'algoritmo di face recognition per trovare le facce
             boxes = face_recognition.face_locations(rgb,model='hog')
-            # compute the facial embedding for the face
+            # codifico il volto e lo salvo in una variabile
             encodings = face_recognition.face_encodings(rgb, boxes)
-            # loop over the encodings
+            # scorro l'array con le codifiche in caso più volti vengano trovati
             for encoding in encodings:
+                # aggiungo la codifica del volto all'array dove le ho tutte
                 knownEncodings.append(encoding)
-                knownNames.append(name)
-knownNames.pop(0)
-conto = 0
-for i in range(len(knownNames)):
-    if i != 0:
-        if knownNames[i] != knownNames[i-1]:
-            conto += 1
-    else:
-        conto += 1
+                # aggiungo il nome corrispondente per riconoscerlo
+                knownNames.append(k)
+            
 
-print(f'conto = {conto}')
-#save emcodings along with their names in dictionary data
+# salvo gli encodings in un dizionario insieme ai nomi corrispondenti
 data = {"encodings": knownEncodings, "names": knownNames}
-#use pickle to save data into a file for later use
-facce = os.path.dirname(os.path.realpath(__file__)) + '\\' + "final_enc"
+# codifico con pickle i dati e li scrivo in un file
+facce = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "encodings.coim"
 f = open(facce, "wb")
 f.write(pickle.dumps(data))
 f.close()
