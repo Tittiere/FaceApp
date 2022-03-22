eea0114753cd0a0091b0ca95337142196f1aeafb
Mattia <78874321+claristorio@users.noreply.github.com>
2022-03-22 11:54:52 +0100

    Update videoRecognition.py

--- videoRecognition.py
+++ videoRecognition.py
@@ -14,58 +14,51 @@ data = pickle.loads(open(encPath, "rb").read())
 print("Streaming started")
 cv2.useOptimized()
 video_capture = cv2.VideoCapture(0)
-# loop over frames from the video file stream
+# ciclo che viene eseguito una volta per ogni frame
 while True:
-    # grab the frame from the threaded video stream
+    # considero il frame attuale
     ret, frame = video_capture.read()
+    # cambio in scala di grigi e do all'algoritmo
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,
-                                         scaleFactor=1.3,
+                                         scaleFactor=1.1,
                                          minNeighbors=5,
-                                         minSize=(30, 30),
+                                         minSize=(60, 60),
                                          flags=cv2.CASCADE_SCALE_IMAGE)
  
-    # convert the input frame from BGR to RGB 
+    # converto il profilo colore da BGR a RGB
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # the facial embeddings for face in input
+    # salvo la codifica del volto
     encodings = face_recognition.face_encodings(rgb)
     names = []
-    # loop over the facial embeddings incase
-    # we have multiple embeddings for multiple fcaes
+    # cerco di riconoscere la faccia/le facce trovate nel frame
     for encoding in encodings:
-       #Compare encodings with encodings in data["encodings"]
-       #Matches contain array with boolean values and True for the embeddings it matches closely
-       #and False for rest
+        # controllo se ho delle corrispondenze nel database di facce
         matches = face_recognition.compare_faces(data["encodings"],encoding)
-        #set name =Unknown if no encoding matches
+        # nome da dare se la persona non è riconosciuta
         name = "Unknown"
-        # check to see if we have found a match
+        # se trovo un match
         if True in matches:
-            #Find positions at which we get True and store them
+            # trovo la posizione nell'array di quella corrispondenza
             matchedIdxs = [i for (i, b) in enumerate(matches) if b]
             counts = {}
-            # loop over the matched indexes and maintain a count for
-            # each recognized face face
+            # ciclo negli id per ogni faccia che corrisponde
             for i in matchedIdxs:
-                #Check the names at respective indexes we stored in matchedIdxs
+                # dopo che ho salvato gli indici delle facce vado a recuperare il nome
                 name = data["names"][i]
-                #increase count for the name we got
                 counts[name] = counts.get(name, 0) + 1
-            #set name which has highest count
-            name = max(counts, key=counts.get)
- 
- 
-        # update the list of names
+            # prendo il nome di chi ha la maggior corrispondenza
+            name = max(counts, key=counts.get) 
+        # aggiungo il nome alla lista di nomi
         names.append(name)
-        # loop over the recognized faces
+        # disegno un quadrato attorno alla faccia riconosciuta
         for ((x, y, w, h), name) in zip(faces, names):
-            # rescale the face coordinates
-            # draw the predicted face name on the image
+            # colore rosso se faccia non riconosciuta
             col = (0, 255, 0)
             if name == 'Unknown':
                 col = (0, 0, 255)
             cv2.rectangle(frame, (x, y), (x + w, y + h), col, 2)
-            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, col, 2)
+            cv2.putText(frame, name, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX,0.75, col, 2)
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
